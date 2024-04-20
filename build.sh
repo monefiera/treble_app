92325522015ca8d3ef041374b7e59b027e17b10e
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2024-04-21 07:42:58 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ if [ -z "$ANDROID_HOME" ];then
     export ANDROID_HOME=$PWD/sdk
 fi
 
-gradleTarget=assembleDebug
-target=debug
-file=app-debug
+gradleTarget=assembleRelease
+target=release
+file=app-release-unsigned
 if [ "$1" == "release" ];then
-    gradleTarget=assembleRelease
-    target=release
-    file=app-release-unsigned
+    gradleTarget=assembleDebug
+    target=debug
+    file=app-debug
 fi
 ./gradlew $gradleTarget
 LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 ./app/build/outputs/apk/$target/${file}.apk TrebleApp.apk
