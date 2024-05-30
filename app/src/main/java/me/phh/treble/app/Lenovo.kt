0a87bb61327a76d074bb7b25402e134f3a36f0bb
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2024-05-30 12:32:37 +0900

    Update Lenovo.kt

--- app/src/main/java/me/phh/treble/app/Lenovo.kt
+++ app/src/main/java/me/phh/treble/app/Lenovo.kt
@@ -10,8 +10,7 @@ import android.util.Log
 import java.io.File
 
 object Lenovo: EntryStartup {
-    val dtPanel = "/sys/devices/virtual/touch/tp_dev/gesture_on" //ZUI13 or older
-    val gestureEnable = "/proc/gesture_control" //ZUI14 or newer
+    val dtPanel = "/sys/devices/virtual/touch/tp_dev/gesture_on" //K5P
     val spListener = SharedPreferences.OnSharedPreferenceChangeListener { sp, key ->
         when(key) {
             LenovoSettings.dt2w -> {
@@ -20,11 +19,6 @@ object Lenovo: EntryStartup {
                 val b = sp.getBoolean(key, false)
                 val value = if(b) "1" else "0"
                 writeToFileNofail(dtPanel, value)
-                try {
-                    val v = if(b) arrayOf<Byte>(-128, 0) else arrayOf<Byte>(0, 0)
-                    File(gestureEnable).outputStream().use { it.write(v.toByteArray()) }
-                } catch(t: Throwable) { Log.d("PHH", "OP: Couldn't write to gesture enable")}
-                Log.d("PHH", "Setting dtPanel to $value")
             }
         }
     }
